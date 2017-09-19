d9b8cfd81e18293ee8dadffa4a9e5d9d2ec7d7d4
haekyu <hkpark627@gmail.com>
2017-09-19 23:56:46 +0900

    initial commit for 0919

---
+++ 0919/0919.py
@@ -0,0 +1,46 @@
+# ㅁ니아로마ㅓㅇ로]
+# 노리ㅕㅑ몬ㅇㄹ
+# ㅗㄴ여ㅑ론일
+# ㅗ냐여로ㅑㅕㄴㅇ라벚ㄷㄹ
+
+
+# for loop
+# result = 0
+# for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+# 	result = result + i
+# print(result)
+
+# result = 0
+# for i in range(1, 11):
+# 	result = result + i
+# print(result)
+
+# while loop
+'''
+while <조건>:
+	<do>
+
+while <i 가 11보다 작을 때>:
+	result = result + i
+'''
+# result = 0
+i = 1
+while i < 11:
+	i += i
+	i += 1
+	# result = result + i
+	# -=
+	# *=
+	# /=
+print(i)
+
+# result = 0
+# result = result + 1
+# result = result + 2
+# result = result + 3
+# result = result + 4
+# result = result + 5
+# result = result + 6
+# result = result + 7
+# result = result + 8
+
